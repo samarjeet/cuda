74149c72b808e5069f222f9a0f86299de6594ef7
Samar <p.samar.j@gmail.com>
2018-11-20 15:14:16 -0500

    device properties

--- b.cu
+++ b.cu
@@ -1,6 +1,16 @@
 #include<iostream>
+#include <cuda.h>
 
 int main(){
   std::cout << "Hello World from CUDA\n";
+  cudaDeviceProp iProp;
+  cudaGetDeviceProperties(&iProp, 2);
+  std::cout << iProp.multiProcessorCount << "\n";
+  std::cout << iProp.totalConstMem/1024.0 << " KB \n";
+  std::cout << iProp.sharedMemPerBlock/1024.0 << " KB \n";
+  std::cout << iProp.regsPerBlock << "  \n";
+  std::cout << iProp.maxThreadsPerBlock << "  \n";
+  std::cout << iProp.maxThreadsPerMultiProcessor << "  \n";
+  std::cout << iProp.maxThreadsPerMultiProcessor/32 << "  \n";
   return 0;
 }
---
+++ cmake-build-debug/CMakeFiles/3.9.0/CMakeCUDACompiler.cmake
@@ -0,0 +1,27 @@
+set(CMAKE_CUDA_COMPILER "/v/apps/cuda/9.2.148/bin/nvcc")
+set(CMAKE_CUDA_HOST_COMPILER "")
+set(CMAKE_CUDA_HOST_LINK_LAUNCHER "g++")
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+set(CMAKE_CUDA_COMPILER_VERSION "9.2.148")
+set(CMAKE_CUDA_STANDARD_COMPUTED_DEFAULT "98")
+set(CMAKE_CUDA_SIMULATE_ID "")
+set(CMAKE_CUDA_SIMULATE_VERSION "")
+
+
+set(CMAKE_CUDA_COMPILER_ENV_VAR "CUDACXX")
+set(CMAKE_CUDA_HOST_COMPILER_ENV_VAR "CUDAHOSTCXX")
+
+set(CMAKE_CUDA_COMPILER_ID_RUN 1)
+set(CMAKE_CUDA_SOURCE_FILE_EXTENSIONS cu)
+set(CMAKE_CUDA_LINKER_PREFERENCE 15)
+set(CMAKE_CUDA_LINKER_PREFERENCE_PROPAGATES 1)
+
+set(CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES "/v/apps/cuda/9.2.148/include")
+
+set(CMAKE_CUDA_HOST_IMPLICIT_LINK_LIBRARIES "cudadevrt;cudart_static;rt;pthread;dl")
+set(CMAKE_CUDA_HOST_IMPLICIT_LINK_DIRECTORIES "/v/apps/cuda/9.2.148/lib64/stubs;/v/apps/cuda/9.2.148/lib64")
+set(CMAKE_CUDA_HOST_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
+
+set(CMAKE_CUDA_IMPLICIT_LINK_LIBRARIES "cudadevrt;cudart_static;rt;pthread;dl;stdc++;m;gcc_s;gcc;c;gcc_s;gcc")
+set(CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES "/v/apps/cuda/9.2.148/lib64/stubs;/v/apps/cuda/9.2.148/lib64;/usr/lib/gcc/x86_64-redhat-linux/4.8.5;/usr/lib64;/lib64;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/ipp/lib/intel64;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/compiler/lib/intel64;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/mkl/lib/intel64;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/tbb/lib/intel64/gcc4.4;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/daal/lib/intel64_lin;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/tbb/lib/intel64_lin/gcc4.4;/v/apps/intel/composer_xe_2016_update3/compilers_and_libraries_2016.3.210/linux/compiler/lib/intel64_lin;/u/samar/Documents/git/caffe2/install/lib;/usr/lib")
+set(CMAKE_CUDA_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
