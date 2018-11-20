9a04374ca5865c2a1aca49062148c2ee988235be
Samar <p.samar.j@gmail.com>
2018-11-20 16:27:56 -0500

    finished matrix sum

--- matrixSum.cu
+++ matrixSum.cu
@@ -21,19 +21,42 @@ int getDeviceProperties(){
   return 0;
 }
 
-__global__ void sumMatrixOnGPU2D(float *A, float *B, float *C, int nx, int ny){
+__global__ void sumMatrixOnGPU2D(float *a, float *b, float *c, int nx, int ny){
   unsigned int ix = blockIdx.x * blockDim.x + threadIdx.x;
   unsigned int iy = blockIdx.y * blockDim.y + threadIdx.y;
 
   unsigned int idx = iy * nx + ix;
   if (ix < nx && iy < ny) {
-    c[ix] = a[ix] + b[ix];
+    c[idx] = a[ix] + b[ix];
   }
 }
 
 int main(int argc, char *argv[]){
   int nx = 1<<14;
   int ny = 1<<14;
+
+
+  float *d_a, *d_b, *d_c;
+  float *h_a, *h_b, *h_c;
+
+  h_a = (float*)malloc(sizeof(float)*nx*ny);
+  h_b = (float*)malloc(sizeof(float)*nx*ny);
+  h_c = (float*)malloc(sizeof(float)*nx*ny);
+
+  for (int i=0 ; i < nx; ++i){
+      for (int j=0; j < ny; ++j){
+          h_a[j * nx + i ] = 1.0;
+          h_b[j * nx + i ] = 2.0;
+      }
+  }
+
+  cudaMalloc(&d_a, sizeof(float)*nx*ny);
+  cudaMalloc(&d_b, sizeof(float)*nx*ny);
+  cudaMalloc(&d_c, sizeof(float)*nx*ny);
+
+  cudaMemcpy(&d_a, &h_a, sizeof(float)*nx*ny, cudaMemcpyHostToDevice);
+  cudaMemcpy(&d_b, &h_b, sizeof(float)*nx*ny, cudaMemcpyHostToDevice);
+
 
   int dimx, dimy;
   if (argc > 2) {
@@ -48,5 +71,7 @@ int main(int argc, char *argv[]){
   dim3 block(dimx, dimy);
   dim3 grid((nx + block.x - 1 )/ block.x, (ny + block.y -1)/block.y);
 
-  return 0; 
+  sumMatrixOnGPU2D<<<grid, block >>>(d_a, d_b, d_c, nx, ny);
+  cudaMemcpy(&h_c, &d_c, sizeof(float)*nx*ny, cudaMemcpyDeviceToHost);
+  return 0;
 }
